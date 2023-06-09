79d68cb9de1ab51de504d09683c706c90111d47a
Oguro Haruka <Oguro Haruka@MSI>
2023-06-09 14:38:33 +0900

    02_07

--- Enemy.h
+++ Enemy.h
@@ -8,11 +8,18 @@ public:
 	void Initialize(Model* model, uint32_t textureHandle);
 	void Update();
 	void Draw(ViewProjection& viewProjection);
+	void Approach(Vector3 move);
+	void Leave(Vector3 move);
+	enum class Phase {
+		Approach,
+		Leave,
+	};
 
 private:
 	WorldTransform worldTransform_;
 	Model* model_ = nullptr;
 	uint32_t textureHandle_ = 0u;
 	Enemy* enemy_ = nullptr;
-	
+	Phase phase_ = Phase::Approach;
+	float kCharacterSpeed;
 };
